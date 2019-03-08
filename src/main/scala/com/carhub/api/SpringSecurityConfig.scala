96c817c39987699966dd11238096d31d07051d15
aelkouhen <Arigatou86*2>
2019-03-09 00:51:35 +0100

    Enhance password encoder

--- src/main/scala/com/carhub/api/SpringSecurityConfig.scala
+++ src/main/scala/com/carhub/api/SpringSecurityConfig.scala
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.{Bean, Configuration}
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
@@ -20,6 +20,6 @@ class SpringSecurityConfig{
 
 
   @Bean
-  def passwordEncoder : PasswordEncoder = new BCryptPasswordEncoder
+  def passwordEncoder : PasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder
 
 }
